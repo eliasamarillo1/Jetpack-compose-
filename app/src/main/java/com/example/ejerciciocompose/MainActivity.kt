a541d64387be87d388b6b34715b944cbad79f329
eliasamarillo <eliasamarilloortiz@gmail.com>
2025-04-09 12:33:06 +0200

    implementamos Scaffold, añadimos toolbar

--- app/src/main/java/com/example/ejerciciocompose/MainActivity.kt
+++ app/src/main/java/com/example/ejerciciocompose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ejerciciocompose
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -17,8 +18,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.AlertDialogDefaults.containerColor
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,26 +39,47 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ejerciciocompose.ui.theme.EjercicioComposeTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            app()
+            ViewContainer()
 
         }
     }
+
+    @Preview
+    @Composable
+    fun ViewContainer() {
+        Scaffold(
+            topBar = {Toolbar()},
+            content = {Content()}
+
+        )
+
+    }
+    @Preview
+    @Composable
+    fun Toolbar(
+    ){
+        TopAppBar(title= {Text(text="Elias profile")},
+            colors =
+                TopAppBarDefaults
+                    .topAppBarColors
+                        (containerColor = colorResource(id = R.color.purple_700)))
+    }
+
     @Preview(showBackground = true)
     @Composable
-    fun app() {
-
-        var counter by rememberSaveable{ mutableStateOf(0)}
-
+    fun Content() {
+        var counter by rememberSaveable { mutableStateOf(0) }
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .background(androidx.compose.ui.graphics.Color.Red)
-                .padding(10.dp)
+                .padding(20.dp)
         ) {
             item {
                 Image(
@@ -63,13 +90,17 @@ class MainActivity : ComponentActivity() {
                     contentDescription = "Imagen"
 
                 )
-                Row(modifier = Modifier.padding(top = 8.dp)){
+                Row(modifier = Modifier.padding(top = 8.dp)) {
                     Image(
-                        painter = painterResource(id=R.drawable.huevandroid),
+                        painter = painterResource(id = R.drawable.huevandroid),
                         contentDescription = null,
-                        modifier = Modifier.clickable {counter++}
+                        modifier = Modifier.clickable { counter++ }
                     )
-                    Text(text=counter.toString(), color= androidx.compose.ui.graphics.Color.White, modifier = Modifier.padding(start = 10.dp))
+                    Text(
+                        text = counter.toString(),
+                        color = androidx.compose.ui.graphics.Color.White,
+                        modifier = Modifier.padding(start = 10.dp)
+                    )
 
                 }
 
@@ -84,21 +115,20 @@ class MainActivity : ComponentActivity() {
                 Text(text = "Elias")
                 Text(text = "No se que decirte")
 
-                LazyRow(modifier = Modifier.fillMaxWidth()
-                    .padding(top = 10.dp)){
+                LazyRow(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 10.dp)
+                ) {
 
-                   item{
-                       Text(text = "Stack: ", color= androidx.compose.ui.graphics.Color.White)
-                       Text(text = " Java ", color= androidx.compose.ui.graphics.Color.White)
-                       Text(text=" Kotlin ", color= androidx.compose.ui.graphics.Color.White)
-
-                   }
-
+                    item {
+                        Text(text = "Stack: ", color = androidx.compose.ui.graphics.Color.White)
+                        Text(text = " Java ", color = androidx.compose.ui.graphics.Color.White)
+                        Text(text = " Kotlin ", color = androidx.compose.ui.graphics.Color.White)
+                    }
                 }
             }
-
         }
-
     }
 }
 
